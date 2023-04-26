42c400038e51c1f6fa2de6bff5ebaba47b41f4e1
shrikantmergu143 <shriakntmergu143@gmail.com>
2023-04-26 23:44:47 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+// Serve static files from the "public" directory
+app.use(express.static('public'));
+
+// Listen for incoming WebSocket connections
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+// Start the server
+const port = process.env.PORT || 3000;
+http.listen(port, () => {
+  console.log(`listening on *:${port}`);
+});
